c458c32e66ec8a6110728d7e9816f5b2eba9df16
shreeramneupane <shreeramneupane0@gmail.com>
2016-02-16 16:09:25 +0545

    PKA-2 Add configuration for Shoulda Matcher

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -49,6 +49,18 @@ Dir[Rails.root.join('app/lib/**/*.rb')].each { |f| require f }
 # If you are not using ActiveRecord, you can remove this line.
 ActiveRecord::Migration.maintain_test_schema!
 
+# Config for Shoulda matcher
+Shoulda::Matchers.configure do |config|
+  config.integrate do |with|
+# Choose a test framework:
+    with.test_framework :rspec
+
+# choose the following (which implies all of the above):
+    with.library :rails
+  end
+end
+
+# Config fot RSpec
 RSpec.configure do |config|
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
