56fc801a38e6fbee034a553599a33ef7d7a89174
shreeramneupane <shreeramneupane0@gmail.com>
2016-02-17 16:27:10 +0545

    PKA-3 Add model Device

---
+++ app/models/device.rb
@@ -0,0 +1,12 @@
+class Device < Item
+  before_create :set_params
+
+  private
+
+  def set_params
+    self.is_readable = false
+    self.is_leaseable = true
+    self.is_rateable = false
+    self.is_reviewable = true
+  end
+end
