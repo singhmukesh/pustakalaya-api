51ebf5921193cd13b06bda095c898cc574c23cf0
shreeramneupane <shreeramneupane0@gmail.com>
2016-02-17 17:30:36 +0545

    PKA-3 Remove validation message containing yml file

--- app/models/publish_detail.rb
+++ app/models/publish_detail.rb
@@ -2,6 +2,5 @@ class PublishDetail < ApplicationRecord
   belongs_to :item
 
   validates :isbn, :author, :publish_date, presence: true
-  validates_date :publish_date, on_or_before: lambda { Date.current },
-  before_message: I18n.t('validates_timeliness.publish_date.before')
+  validates_date :publish_date, on_or_before: lambda { Date.current }
 end
