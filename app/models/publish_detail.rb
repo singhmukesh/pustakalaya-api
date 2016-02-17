bb240fb391ec04bdf6b53eaff0db459871cf40d7
shreeramneupane <shreeramneupane0@gmail.com>
2016-02-17 17:27:20 +0545

    PKA-3 Validate PublishDetail model publish date

--- app/models/publish_detail.rb
+++ app/models/publish_detail.rb
@@ -2,4 +2,6 @@ class PublishDetail < ApplicationRecord
   belongs_to :item
 
   validates :isbn, :author, :publish_date, presence: true
+  validates_date :publish_date, on_or_before: lambda { Date.current },
+  before_message: I18n.t('validates_timeliness.publish_date.before')
 end
