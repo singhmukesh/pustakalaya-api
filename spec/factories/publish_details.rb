9c26571c9c0f4ab986bd89442044ec4cecf33fbd
shreeramneupane <shreeramneupane0@gmail.com>
2016-02-29 09:52:40 +0545

    PKA-8 Refactor publish_detail factories

--- spec/factories/publish_details.rb
+++ spec/factories/publish_details.rb
@@ -1,7 +1,7 @@
 FactoryGirl.define do
   factory :publish_detail do
     isbn { Faker::Code.isbn }
-    author { Faker::Book.author }
+    author { Faker::Name.name }
     publish_date { Faker::Date.backward }
   end
 
