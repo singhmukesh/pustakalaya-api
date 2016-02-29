b177965fe42bf544a0d6ad685c3b21ddd454e5fa
shreeramneupane <shreeramneupane0@gmail.com>
2016-02-29 11:13:35 +0545

    PKA-11 Create set_lease private method in UserMailer

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -4,9 +4,7 @@ class UserMailer < ApplicationMailer
   #
   # @params lease_id [Lease::ActiveRecord_Relation id attribute]
   def lease_success(lease_id)
-    @lease = Lease.find(lease_id)
-    @user = @lease.user
-    @item = @lease.item
+    set_lease(lease_id)
 
     mail(from: ENV['MAILER_EMAIL'], to: @user.email, subject: t('user_mailer.lease_success.subject'))
   end
@@ -15,10 +13,16 @@ class UserMailer < ApplicationMailer
   #
   # @params lease_id [Lease::ActiveRecord_Relation id attribute]
   def return_success(lease_id)
-    @lease = Lease.find(lease_id)
-    @item = @lease.item
-    @user = @lease.user
+    set_lease(lease_id)
 
     mail(from: ENV['MAILER_EMAIL'], to: @user.email, subject: t('user_mailer.return_success.subject'))
+  end
+
+  private
+
+  def set_lease(id)
+    @lease = Lease.find(id)
+    @user = @lease.user
+    @item = @lease.item
   end
 end
