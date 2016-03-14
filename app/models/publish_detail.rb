b57b5d2052199adeaa8a5b9495d7e9424a964d87
shreeramneupane <shreeramneupane0@gmail.com>
2016-03-14 14:44:19 +0545

    PKA-38 Remove PublishDetail's publish_date attribute required validation

--- app/models/publish_detail.rb
+++ app/models/publish_detail.rb
@@ -2,5 +2,5 @@ class PublishDetail < ApplicationRecord
   belongs_to :item
 
   validates :author, presence: true
-  validates_date :publish_date, on_or_before: lambda { Date.current }
+  validates_date :publish_date, on_or_before: lambda { Date.current }, allow_nil: true
 end
